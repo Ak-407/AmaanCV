1ae80cbf93f9a1f78da2872390cfcecb7cb60be8
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-02-26 21:05:18 +0530

    Update project.jsx

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -17,7 +17,7 @@ function click() {
   <div class="navbar">
     <a href="https://weather-phi-five.vercel.app/" class="navbar-link"><i class="fa-solid fa-cloud-sun-rain"></i></a>
     <a href="https://ak-407.github.io/Voice-Text-Converter-/" class="navbar-link"><i class="fa-brands fa-teamspeak"></i></a>
-    <a href="https://ak-407.github.io/Ask-Me-/" class="navbar-link"><i class="fa-solid fa-robot"></i></a>
+    <a href="https://ak-407.github.io/Chat-GPT/" class="navbar-link"><i class="fa-solid fa-robot"></i></a>
     <a href="https://syedamaan.vercel.app/" class="navbar-link"><i class="fa-solid fa-person-shelter"></i></a>
     <a href="https://blogpost-1b1k.onrender.com/" class="navbar-link"><i class="fa-regular fa-id-badge"></i></a>
     <a href="https://ak-407.github.io/Contact-Me/" class="navbar-link"><i class="fa-sharp fa-solid fa-blog"></i></a>
